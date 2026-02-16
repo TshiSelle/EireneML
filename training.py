7b1fda5da4f88a7d02f94b68fdee41c186d7c458
Omar Houssami <omar.houssami20@gmail.com>
2026-02-16 21:55:27 +0200

    Fixed training data processing and model saving Uses normalized words for vocabulary, fixes bag-of-words construction, prevents array shape issues, updates optimizer compatibility, and saves the model correctly. Improves training reliability and model consistency.

--- training.py
+++ training.py
@@ -31,16 +31,13 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# print(documents)
-
-word = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
-words = sorted(set(words))
+word = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(set(word))
 
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
-# print(words)
 
 training = []
 output_empty = [0] * len(classes)
@@ -48,16 +45,17 @@ output_empty = [0] * len(classes)
 for document in documents:
     bag = []
     word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
-    for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
+    word_patterns = [lemmatizer.lemmatize(w.lower()) for w in word_patterns]
+
+    for w in words:
+        bag.append(1 if w in word_patterns else 0)
 
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
@@ -69,10 +67,14 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+try:
+    sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+except TypeError:
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('eirene.h5', hist)
+
+model.save('eirene.h5')
 
 print("Done")
